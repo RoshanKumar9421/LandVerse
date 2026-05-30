b735debca36fd2681441e2a98f6e02677cd8bda1
RoshanKumar9421 <yadavroshankumar8271@gmail.com>
2026-05-30 15:39:14 +0530

    pages added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import OTPVerificationPage from './pages/OTPVerificationPage';
-import ConnectWalletPage from './pages/ConnectWalletPage';
-import KYCVerificationPage from './pages/KYCVerificationPage';
-import DashboardPage from './pages/DashboardPage';
+import { routes } from './routes';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/verify" element={<OTPVerificationPage />} />
-        <Route path="/connect-wallet" element={<ConnectWalletPage />} />
-        <Route path="/kyc" element={<KYCVerificationPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
+        {routes.map((route, index) => (
+          <Route key={index} path={route.path} element={route.element} />
+        ))}
       </Routes>
     </BrowserRouter>
   );
